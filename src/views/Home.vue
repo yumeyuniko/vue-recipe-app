8b48d6cd0528c653acbcc60f8f5ec3a5a89b561b
yumeyuniko <daigocafe@msn.com>
2022-01-20 02:08:26 +0900

    フォーム追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="home">
     <h1>My Recipes</h1>
+    <button>Add new Recipe</button>
+
+    <div class="recipes"></div>
+    <!-- RECIPE TO GO HERE -->
+
+    <div class="add-recipe-popup">
+      <div class="popup-content">
+        <h2>Add new recipe</h2>
+
+        <form @submit.prevent="">
+          <div class="group">
+            <label>Title</label>
+            <input type="text" />
+          </div>
+
+          <div class="group">
+            <label>Description</label>
+            <textarea></textarea>
+          </div>
+
+          <div class="group">
+            <label>Ingredients</label>
+            <div class="ingredient">
+              <input type="text" />
+            </div>
+            <button type="button">Add Ingredient</button>
+          </div>
+
+          <div class="group">
+            <label>Method</label>
+            <div class="method">
+              <textarea></textarea>
+            </div>
+            <button type="button">Add step</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
